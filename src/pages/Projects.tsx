053421b26d12997700fcf29cd4e8da6b856beccf
Emily Flynn <emily.flynn@softwire.com>
2023-08-30 11:45:03 +0100

    Styling

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,18 +1,21 @@
 import projects from '../data/projects'
+import './Projects.css'
 
 function Projects() {
   return (
-    <>
+    <div className="Projects">
       <h1>My Projects</h1>
       {projects.map((project) =>
-        <div>
-          <h2>{project.name}</h2>
-          <p>{project.description}</p>
-          <img alt={`${project.name} screenshot`} src={project.imageUrl} />
-          {project.link && <a href={project.link} target="_blank">See this project live</a>}
+        <div className="Projects__Item">
+          <img alt={`${project.name} screenshot`} src={project.imageUrl} width={160} height={90} className="Projects__Image"/>
+          <div>
+            <h2>{project.name}</h2>
+            <p>{project.description}</p>
+            {project.link && <a href={project.link} target="_blank">See this project live</a>}
+          </div>
         </div>
       )}
-    </>
+    </div>
   )
 }
 
